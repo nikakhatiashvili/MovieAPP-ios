5aa8e61bc2ca15256881dfebb2f976764810bb8d
nika khatiashvili <nika.khatiashvili@makingscience.com>
2023-07-16 15:17:55 +0400

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by user on 7/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
